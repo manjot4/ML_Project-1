c48c123b40ff1c670869e10c761e33009cd528c0
root <root@iccluster027.iccluster.epfl.ch>
2019-10-12 01:41:51 +0200

    Refactor

--- preprocessing.py
+++ preprocessing.py
@@ -145,7 +145,7 @@ PRI_jet_num_features = {
 }
 
 
-def __DER_mass_MMC_split(y, X, ids):
+def __DER_mass_MMC_split(y, tx, ids):
     """ Splits a "PRI_jet_num" subset into two subsets. One contains the "DER_mass_MMC" feature
     with defined values and in the other the "DER_mass_MMC" feature is dropped.
 
@@ -153,7 +153,7 @@ def __DER_mass_MMC_split(y, X, ids):
     ---------
     y: ndarray
         The target variables
-    X: ndarray
+    tx: ndarray
         The dataset
     ids: ndarray
         The ids
@@ -164,22 +164,22 @@ def __DER_mass_MMC_split(y, X, ids):
         The two subsets
     """
 
-    # Find the rows in X where the "DER_mass_MMC" feature has the
+    # Find the rows in tx where the "DER_mass_MMC" feature has the
     # defined values.
-    r = X[:, features["DER_mass_MMC"]] != -999
+    r = tx[:, features["DER_mass_MMC"]] != -999
 
     # Create the first subset extracting the rows with the defined
     # "DER_mass_MMC" feature values. 
-    ds_1 = (y[r], X[r], ids[r])
+    ds_1 = (y[r], tx[r], ids[r])
 
     # Create the second subset extracting the rows with the undefined
     # "DER_mass_MMC" feature values and drop the "DER_mass_MMC" feature.
-    ds_2 = (y[~r], X[~r, 1:], ids[~r])
+    ds_2 = (y[~r], tx[~r, 1:], ids[~r])
 
     return ds_1, ds_2
 
 
-def __PRI_jet_num_split(y, X, ids, PRI_jet_num_vals):
+def __PRI_jet_num_split(y, tx, ids, PRI_jet_num_vals):
     """ Splits a dataset using the "PRI_jet_num" feature removing the
     unnecessary nan values.
 
@@ -187,7 +187,7 @@ def __PRI_jet_num_split(y, X, ids, PRI_jet_num_vals):
     ----------
     y: ndarray
         The target variables
-    X: ndarray
+    tx: ndarray
         The dataset
     ids: ndarray
         The ids
@@ -197,16 +197,16 @@ def __PRI_jet_num_split(y, X, ids, PRI_jet_num_vals):
     Returns
     -------
     array
-        Array of tuples each containing subsets of y, X, and ids
+        Array of tuples each containing subsets of y, tx, and ids
     """
     res = []
 
     # Iterate over each possible value for "PRI_jet_num".
     for PRI_jet_num in PRI_jet_num_vals:
         
-        # Find the rows in X where the "PRI_jet_num" feature has the current
+        # Find the rows in tx where the "PRI_jet_num" feature has the current
         # PRI_jet_num value.
-        r = X[:, features["PRI_jet_num"]] == PRI_jet_num
+        r = tx[:, features["PRI_jet_num"]] == PRI_jet_num
         
         # Extract the features measured by the current PRI_jet_num. Discard
         # the others since they only contain nan values.
@@ -214,7 +214,7 @@ def __PRI_jet_num_split(y, X, ids, PRI_jet_num_vals):
 
         # Split the dataset further since the "DER_mass_MMC" feature has
         # too much nan values.
-        ds_1, ds_2 = __DER_mass_MMC_split(y[r], X[r][:, f], ids[r])
+        ds_1, ds_2 = __DER_mass_MMC_split(y[r], tx[r][:, f], ids[r])
 
         # Append the newly created datasets.
         res.append(ds_1)
@@ -223,14 +223,14 @@ def __PRI_jet_num_split(y, X, ids, PRI_jet_num_vals):
     return res
 
 
-def PRI_jet_num_split(y, X, ids, combine_vals=False):
+def PRI_jet_num_split(y, tx, ids, combine_vals=False):
     """ Wrapper around "__PRI_jet_num_split" that does the actual split.
 
     Parameters
     ----------
     y: ndarray
         The target variables
-    X: ndarray
+    tx: ndarray
         The dataset
     ids: ndarray
         The ids
@@ -240,12 +240,12 @@ def PRI_jet_num_split(y, X, ids, combine_vals=False):
     Returns
     -------
     array
-        Array of tuples each containing subsets of y, X, and ids
+        Array of tuples each containing subsets of y, tx, and ids
     """
 
     # Copy the sets so that the original data is preserved.
     y_cpy = np.copy(y)
-    X_cpy = np.copy(X)
+    X_cpy = np.copy(tx)
     ids_cpy = np.copy(ids)
 
     # Combine values 2 and 3 by setting the value 3 to 2.
@@ -334,12 +334,12 @@ def minmax_normalize(X_train, X_test):
 class WrongModeException(Exception): pass
 
 
-def __clean_nan(X, mode):
-    """ Replaces the nan values in X with the calculated value by mode.
+def __clean_nan(tx, mode):
+    """ Replaces the nan values in tx with the calculated value by mode.
 
     Parameters
     ----------
-    X: ndarray
+    tx: ndarray
         The dataset
     mode: function
         np.mean or np.median
@@ -350,23 +350,23 @@ def __clean_nan(X, mode):
         Cleaned dataset
     """
     # Create a mask for the valid values.
-    mask = X != -999
-    # Remove the nan values from X.
-    x = X * mask
+    mask = tx != -999
+    # Remove the nan values from tx.
+    x = tx * mask
     # Create a matrix that contains only the mean/median values in the
     # entries where there was nan value in the originl matrix.
-    y = np.ones(X.shape) * ~mask * mode(x[np.nonzero(x)], axis=0)
+    y = np.ones(tx.shape) * ~mask * mode(x[np.nonzero(x)], axis=0)
     # Add the mean value entries to the matrix.
     return x + y 
 
 
-def clean_nan(X, mode="mean"):
-    """ Wrapper around the function that replaces the nan values in X
+def clean_nan(tx, mode="mean"):
+    """ Wrapper around the function that replaces the nan values in tx
     with the calculated value by mode.
 
     Parameters
     ----------
-    X: ndarray
+    tx: ndarray
         The dataset
     mode: string
         "mean" or "median"
@@ -377,9 +377,25 @@ def clean_nan(X, mode="mean"):
         Cleaned dataset
     """
     if mode == "mean":
-        return __clean_nan(X, np.mean)
+        return __clean_nan(tx, np.mean)
 
     if mode =="median":
-        return __clean_nan(X, np.median)
+        return __clean_nan(tx, np.median)
 
     raise WrongModeException
+
+
+def map_0_1(y):
+    y_copy = y.copy()
+
+    y_copy[y_copy == -1] == 0
+
+    return y_copy
+
+
+def map_minus_1_1(y):
+    y_copy = y.copy()
+
+    y_copy[y_copy == 0] == -1
+
+    return y_copy
