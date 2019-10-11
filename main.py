34fdc0a461c8d046add64661a9f600306d7c50f5
root <root@iccluster027.iccluster.epfl.ch>
2019-10-12 01:41:58 +0200

    Update main

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ test_fname = "data/test.csv"
 y_train, X_train, ids_train = load_csv_data(train_fname)
 y_test, X_test, ids_test = load_csv_data(test_fname)
 
+print("Shapse")
 print(X_train.shape, y_train.shape, ids_train.shape)
 print(X_test.shape, y_test.shape, ids_test.shape)
+print()
 
 from preprocessing import PRI_jet_num_split
 from preprocessing import standardize, minmax_normalize
 from preprocessing import clean_nan
-from implementations import least_squares_SGD
+from preprocessing import map_0_1, map_minus_1_1
+from implementations import reg_logistic_regression
 from scripts.helpers import predict_labels
 
 combine_vals = False
@@ -37,6 +40,9 @@ y_pred = []
 for i in range(num_subsets):
     y_train_subset, X_train_subset, ids_train_subset = train_subsets[i]
     y_test_subset, X_test_subset, ids_test_subset = test_subsets[i]
+
+    y_train_subset = map_0_1(y_train_subset)
+    y_test_subset = map_0_1(y_test_subset)
     
     X_train_subset, X_test_subset = standardize(X_train_subset, X_test_subset)
 
@@ -44,14 +50,19 @@ for i in range(num_subsets):
 
     initial_w = np.random.randn(D)
     gamma = 0.003
+    lambda_ = 0.1
     
     print(f"Train shape: {str(X_train_subset.shape):>12}   Test shape: {str(X_test_subset.shape):>12}")
+    print()
     
-    loss, w = least_squares_SGD(y_train_subset, X_train_subset, initial_w, 1, N, gamma)
-    labels = predict_labels(w, X_test_subset)
+loss, w = reg_logistic_regression(y_train_subset, X_train_subset, lambda_, initial_w, 100, gamma)
+labels = predict_labels(w, X_test_subset)
 
-    print(f"Number of samples:                      { len(labels) }")
-    print(f"Number of correctly classified samples: { np.sum(labels - y_test_subset == 0) }")
-    
-    ids.extend(ids_test_subset)
-    y_pred.extend(labels)
+labels = map_minus_1_1(labels)
+
+print(f"Number of samples:                      { len(labels) }")
+print(f"Number of correctly classified samples: { np.sum(labels - y_test_subset == 0) }")
+print("\n")    
+
+ids.extend(ids_test_subset)
+y_pred.extend(labels)
