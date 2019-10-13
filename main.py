475a04d805424c0fd3a7d7e38edbcc16532075eb
korunosk <mladen.korunoski@epfl.ch>
2019-10-13 12:37:18 +0200

    Add baseline gamma and lambda; sorted y_pred

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import numpy as np
+from preprocessing import PRI_jet_num_split
+from preprocessing import standardize, minmax_normalize
+from preprocessing import clean_nan
+from preprocessing import map_0_1, map_minus_1_1
+from implementations import reg_logistic_regression
 from scripts.helpers import load_csv_data
+from scripts.helpers import predict_labels, create_csv_submission
 
 np.random.seed(1)
 
+gammas = [1e-6, 1e-6, 1e-6, 1e-5, 1e-6, 1e-05, 1e-05, 1e-05]
+lambdas_ = [1000.0, 0.001, 100.0, 0.001, 100.0, 100.0, 0.001, 100.0]
+
 train_fname = "data/train.csv"
 test_fname = "data/test.csv"
+sumbission_fname = "data/submission.csv"
 
 y_train, X_train, ids_train = load_csv_data(train_fname)
 y_test, X_test, ids_test = load_csv_data(test_fname)
@@ -13,13 +23,6 @@ print("Shapes")
 print(X_train.shape, y_train.shape, ids_train.shape)
 print(X_test.shape, y_test.shape, ids_test.shape)
 print()
-
-from preprocessing import PRI_jet_num_split
-from preprocessing import standardize, minmax_normalize
-from preprocessing import clean_nan
-from preprocessing import map_0_1, map_minus_1_1
-from implementations import reg_logistic_regression
-from scripts.helpers import predict_labels
 
 combine_vals = False
 
@@ -34,35 +37,37 @@ assert len(train_subsets) == len(test_subsets)
 
 num_subsets = len(train_subsets)
 
-ids = []
-y_pred = []
+ids = np.array([])
+y_pred = np.array([])
+
+def sort_arr(ids, y_pred):
+    idx = ids.argsort()
+    return ids[idx], y_pred[idx]
 
 for i in range(num_subsets):
     y_train_subset, X_train_subset, ids_train_subset = train_subsets[i]
     y_test_subset, X_test_subset, ids_test_subset = test_subsets[i]
 
     y_train_subset = map_0_1(y_train_subset)
-    y_test_subset = map_0_1(y_test_subset)
-    
+
     X_train_subset, X_test_subset = standardize(X_train_subset, X_test_subset)
 
     N, D = X_train_subset.shape
 
     initial_w = np.random.randn(D)
-    gamma = 0.003
-    lambda_ = 0.1
-    
+    gamma = gammas[i]
+    lambda_ = lambdas_[i]
+
     print(f"Train shape: {str(X_train_subset.shape):>12}   Test shape: {str(X_test_subset.shape):>12}")
     print()
-    
-loss, w = reg_logistic_regression(y_train_subset, X_train_subset, lambda_, initial_w, 100, gamma)
-labels = predict_labels(w, X_test_subset)
+        
+    loss, w = reg_logistic_regression(y_train_subset, X_train_subset, lambda_, initial_w, 2000, gamma)
 
-labels = map_minus_1_1(labels)
+    y_pred_test = np.array(map_minus_1_1(predict_labels(w, X_test_subset)))
 
-print(f"Number of samples:                      { len(labels) }")
-print(f"Number of correctly classified samples: { np.sum(labels - y_test_subset == 0) }")
-print("\n")    
+    ids = np.concatenate((ids, ids_test_subset))
+    y_pred = np.concatenate((y_pred, y_pred_test))
 
-ids.extend(ids_test_subset)
-y_pred.extend(labels)
+ids, y_pred = sort_arr(ids, y_pred)
+
+create_csv_submission(ids, y_pred, sumbission_fname)
